0e958121bfc4a86165753e9692acac4b436ef44f
braxxypedroceolato <pedro.ceolato@braxxy.com.br>
2023-05-17 16:23:13 -0300

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', () => {
+  const users = prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server listening on port http://localhost:3333')
+  })
---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="text-2xl font-bold uppercase w-20 h-20 bg-red-500 rounded m-10">
+      oi
+    </h1>
+  )
+}
